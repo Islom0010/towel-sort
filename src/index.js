007f28a50c65a8d4bcc80f77639017e144ecbcb9
Islom <ulmasov_i@yahoo.com>
2022-08-02 23:12:48 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
+const x = [
+  [1, 2, 3, 4],
+  [7, 6, 5, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
 module.exports = function towelSort (matrix) {
-  return [];
+  let l = matrix.length;
+  let arr = [];
+  for(let i = 0; i < l; i++) {
+    arr.push(...matrix[i])
+  }
+  return console.log(arr);
 }
+
+towelSort(x);
